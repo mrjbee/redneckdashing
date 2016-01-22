b86edb08d1687b9cf3c1318d79e9ac830d552a54
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2016-01-22 04:02:04 +0200

    [ci] update colors

--- jobs/jenkins.rb
+++ jobs/jenkins.rb
@@ -14,12 +14,23 @@ job_mapping = {
 }
 
 job_mapping.each do |title, job|
-  SCHEDULER.every job[:update], :first_in => 0 do
+  SCHEDULER.every job[:update], :first_in => rand(5)  do
     current_job_details = Jenkins.lastJobStatus(job[:server], job[:title])
     puts "Just fetched job = #{current_job_details}"
+    state = 0
+
+    if current_job_details.is_fail
+      state = -1
+    end
+
+    if current_job_details.is_over_time
+      state = -2
+    end
+
     send_event( title,   {
       value: current_job_details.progress_human,
       startAtValue: current_job_details.start_at,
-      urlValue: current_job_details.url })
+      urlValue: current_job_details.url,
+      healthStateValue: state })
   end
 end
--- widgets/ci/ci.coffee
+++ widgets/ci/ci.coffee
@@ -16,3 +16,16 @@ class Dashing.Ci extends Dashing.Widget
     ci.attr("data-bgcolor", ci.css("background-color"))
     ci.attr("data-fgcolor", ci.css("color"))
     ci.knob()
+
+  onData: (data) ->
+    console.log(data.healthStateValue)
+    $(@node).fadeOut =>
+      if data.healthStateValue == 0
+        $(@node).css('background-color', '#9c4274')
+        #when failed
+      if data.healthStateValue == -1
+        $(@node).css('background-color', '#4b4b4b')
+        #when over running
+      if data.healthStateValue == -2
+        $(@node).css('background-color', '#64334f')
+    $(@node).fadeIn()
