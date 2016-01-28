43c728c1907eac0d3d485e9f3dd5dbcf5f0c04b8
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2016-01-28 14:56:16 +0200

    Diasble watcher untill UI implementation

--- jobs/gitlab_MR_watcher.rb
+++ jobs/gitlab_MR_watcher.rb
@@ -1,9 +1,9 @@
 require_relative '../lib/jenkins.rb'
 require_relative '../lib/setup.rb'
 
-Setup.gitLab_open_MR.each do |title, details|
-
-  SCHEDULER.every details[:update], :first_in => rand(15)  do
-
-  end
-end
+#Setup.gitLab_open_MR.each do |title, details|
+#
+#  SCHEDULER.every details[:update], :first_in => rand(15)  do
+#
+#  end
+#end
